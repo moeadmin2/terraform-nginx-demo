b532bf2daea4da39827cb4ea5b0c45a238a9fdc1
G Yeo <glennyeo.2018@scis.smu.edu.sg>
2025-06-25 17:36:46 +0800

    added aws provider

---
+++ main.tf
@@ -0,0 +1,11 @@
+
+
+
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws" # taken from https://registry.terraform.io/providers/hashicorp/aws/latest
+            version = "~> 6.0"
+        }
+    }
+}
