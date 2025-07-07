6d8193ae29ed50fa2043ecccd6dc4bc47c52ad3a
G Yeo <glennyeo.2018@scis.smu.edu.sg>
2025-07-07 12:12:14 +0800

    1. removed bastion hosts 2. removed ALB 3. re-added NLB 4. restricted VPC CIDR range to certain IPv4 ranges in RFC 1918 5. tidied up some code

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 # terraform setup
 
-# Note - due to troubleshooting difficulties with NLBs, I used bastion host to enter the nginx host. I limited it to my own IP address
-
 terraform {
   required_providers {
     aws = {
@@ -18,7 +16,7 @@ provider "aws" {
 
 # creating a VPC
 resource "aws_vpc" "main" {
-  cidr_block = "118.189.0.0/16" # allows DNS resolution inside my VPC
+  cidr_block = "10.0.0.0/8" # Best practice to choose RFC 1918 private ranges with 16 subnets: https://docs.aws.amazon.com/vpc/latest/userguide/vpc-cidr-blocks.html
 
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -26,9 +24,9 @@ resource "aws_vpc" "main" {
 
 # subnet a inside the vpc
 resource "aws_subnet" "subnet_a" {
-  vpc_id                  = aws_vpc.main.id # puts subnet into vpc
-  cidr_block              = "118.189.1.0/24"
-  availability_zone       = "ap-southeast-1a"
+  vpc_id            = aws_vpc.main.id # puts subnet into vpc
+  cidr_block        = "118.189.1.0/24"
+  availability_zone = "ap-southeast-1a"
 
   tags = {
     name = "subnet-a-public"
@@ -37,9 +35,9 @@ resource "aws_subnet" "subnet_a" {
 
 # subnet b inside the vpc
 resource "aws_subnet" "subnet_b" {
-  vpc_id                  = aws_vpc.main.id # puts subnet into vpc
-  cidr_block              = "118.189.2.0/24"
-  availability_zone       = "ap-southeast-1b"
+  vpc_id            = aws_vpc.main.id # puts subnet into vpc
+  cidr_block        = "118.189.2.0/24"
+  availability_zone = "ap-southeast-1b"
 
   tags = {
     name = "subnet-b-private"
@@ -73,7 +71,7 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id = aws_subnet.subnet_a.id
+  subnet_id     = aws_subnet.subnet_a.id
 }
 
 # I am trying to give private subnet B outbound internet access so the curl/apt install can work, but i dont want to assign a public IP to these instance to comply with the requirement(s)
@@ -83,14 +81,14 @@ resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   }
 }
 
 # binding subnet B to that custom routing table I made earlier, if not subnet B may use the default routing table that may not have NAT and block access
 resource "aws_route_table_association" "private" {
-  subnet_id = aws_subnet.subnet_b.id
+  subnet_id      = aws_subnet.subnet_b.id
   route_table_id = aws_route_table.private_rt.id
 }
 
@@ -114,26 +112,26 @@ resource "aws_route_table_association" "a" {
 resource "aws_security_group" "web_sg" {
   vpc_id = aws_vpc.main.id
 
-  # to allow ingress from SSH port 22 based on req3, 
-
-  # ssh
   ingress {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
-    # given in the requirements
-    # cidr_blocks = ["118.189.0.0/16"]
-    cidr_blocks = ["118.189.0.0/16" ]
+    cidr_blocks = [
+      "118.189.0.0/16",   # SSH per TA.pdf
+      # "0.0.0.0/0" # your IP for testing
+    ]
   }
 
-  # to allow ingress from HTTP port 80 based on req3,
-  # http
   ingress {
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
-    # given in the requirements
-    cidr_blocks = ["118.189.0.0/16", "116.206.0.0/16", "223.25.0.0/16"]
+    cidr_blocks = [
+      "118.189.0.0/16", # HTTP per TA.pdf
+      "116.206.0.0/16",
+      "223.25.0.0/16",
+      # "0.0.0.0/0" # your IP for testing
+    ]
   }
 
   egress {
@@ -144,6 +142,7 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+
 #didnt put egress since there are no outbound restrictions
 
 # after creating the vpc, security group, then we create the ec2 instance as needed in req.2
@@ -152,21 +151,6 @@ resource "aws_security_group" "web_sg" {
 data "aws_ssm_parameter" "ubuntu_24_04" {
   name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
 }
-
-# data "aws_ami" "ubuntu_24_04" {  # if the question specifically want 24.04 LTS OS
-#   most_recent = true             # as tested in aws, get the latest compatible ami
-#   owners      = ["099720109477"] # referenced from here: https:#documentation.ubuntu.com/aws/aws-how-to/instances/find-ubuntu-images/ canonical should be official publisher
-
-#   filter {
-#     name   = "image-id"
-#     values = [data.aws_ssm_parameter.ubuntu_24_04.value]
-#   }
-
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-# }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "ec2-ssh-key"
@@ -211,162 +195,63 @@ docker cp /tmp/index.html nginx:/usr/share/nginx/html/index.html
 EOF
 }
 
-
-
-resource "aws_instance" "bastion" {
-  ami                         = data.aws_ssm_parameter.ubuntu_24_04.value
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.subnet_a.id
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
-  key_name                    = aws_key_pair.deployer.key_name
-
-  tags = {
-    Name = "bastion-host"
-  }
-
-  user_data = <<-EOF
-#!/bin/bash
-apt update -y
-apt install -y curl net-tools
-EOF
+resource "aws_lb" "web_nlb" {
+  name                       = "web-nlb"
+  internal                   = false
+  load_balancer_type         = "network"
+  subnets                    = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+  enable_deletion_protection = false
 }
 
-
-resource "aws_security_group" "bastion_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["158.140.x.x/32"] # my own IP address - testing purposes
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# adding target group for SSH
+resource "aws_lb_target_group" "tg_ssh" {
+  name              = "ssh-target-group"
+  port              = 22
+  protocol          = "TCP"
+  vpc_id            = aws_vpc.main.id
+  target_type       = "instance"
+  proxy_protocol_v2 = false
 }
 
-
-resource "aws_lb" "web_alb" {
-  name               = "web-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
-}
-
-resource "aws_security_group" "alb_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["118.189.0.0/16", "116.206.0.0/16", "223.25.0.0/16"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-resource "aws_lb_target_group" "http_tg" {
-  name        = "http-target"
+# adding target group for HTTP
+resource "aws_lb_target_group" "tg_http" {
+  name        = "http-target-group"
   port        = 80
-  protocol    = "HTTP"
+  protocol    = "TCP"
   vpc_id      = aws_vpc.main.id
   target_type = "instance"
-  health_check {
-    path     = "/"
-    protocol = "HTTP"
-  }
+}
+
+resource "aws_lb_target_group_attachment" "ssh_attachment" {
+  target_id        = aws_instance.nginx.id
+  target_group_arn = aws_lb_target_group.tg_ssh.arn
+  port             = 22
 }
 
 resource "aws_lb_target_group_attachment" "http_attachment" {
-  target_group_arn = aws_lb_target_group.http_tg.arn
   target_id        = aws_instance.nginx.id
+  target_group_arn = aws_lb_target_group.tg_http.arn
   port             = 80
 }
 
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.web_alb.arn
-  port              = 80
-  protocol          = "HTTP"
+resource "aws_lb_listener" "ssh_listener" {
+  load_balancer_arn = aws_lb.web_nlb.arn
+  port              = 22
+  protocol          = "TCP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.http_tg.arn
+    target_group_arn = aws_lb_target_group.tg_ssh.arn
   }
 }
 
-# I am unable to make the NLB work - reverting back to bastion host for easier entry to SSH
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.web_nlb.arn
+  port              = 80
+  protocol          = "TCP"
 
-# resource "aws_lb" "web_nlb" {
-#   name                       = "web-nlb"
-#   internal                   = false
-#   load_balancer_type         = "network"
-#   subnets                    = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
-#   enable_deletion_protection = false
-# }
-
-# # adding target group for SSH
-# resource "aws_lb_target_group" "tg_ssh" {
-#   name        = "ssh-target-group"
-#   port        = 22
-#   protocol    = "TCP"
-#   vpc_id      = aws_vpc.main.id
-#   target_type = "instance"
-#   proxy_protocol_v2 = false
-# }
-
-# # adding target group for HTTP
-# resource "aws_lb_target_group" "tg_http" {
-#   name        = "http-target-group"
-#   port        = 80
-#   protocol    = "TCP"
-#   vpc_id      = aws_vpc.main.id
-#   target_type = "instance"
-# }
-
-# resource "aws_lb_target_group_attachment" "ssh_attachment" {
-#   target_id        = aws_instance.nginx.id
-#   target_group_arn = aws_lb_target_group.tg_ssh.arn
-#   port             = 22
-# }
-
-# resource "aws_lb_target_group_attachment" "http_attachment" {
-#   target_id        = aws_instance.nginx.id
-#   target_group_arn = aws_lb_target_group.tg_http.arn
-#   port             = 80
-# }
-
-# resource "aws_lb_listener" "ssh_listener" {
-#   load_balancer_arn = aws_lb.web_nlb.arn
-#   port              = 22
-#   protocol          = "TCP"
-
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.tg_ssh.arn
-#   }
-# }
-
-# resource "aws_lb_listener" "http_listener" {
-#   load_balancer_arn = aws_lb.web_nlb.arn
-#   port              = 80
-#   protocol          = "TCP"
-
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.tg_http.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_http.arn
+  }
+}
